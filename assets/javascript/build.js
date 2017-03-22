c6fc9edb5f5b007fa0a3fb5d4607b89473074d0a
Nathaniel Long <nathaniel.s.long@gmail.com>
2017-03-22 14:53:53 -0400

    working on html and added image file

--- assets/javascript/build.js
+++ assets/javascript/build.js
@@ -1,7 +1,11 @@
+$(document).ready(function() {
+  buildBoard();
+})
+
 // in the div board, append divs so that 10 fit in each row
 function buildBoard() {
   for (i = 0; i < 100; i++) {
-    $('.board').append("<div id=position-" + i + " data-index=" + i + "></div>")
+    $('.board').append("<div class='cell' id=position-" + i + " data-index=" + i + "></div>")
     if (Math.floor(i / 10) % 2 == 0) {
       if (i % 2 == 0) {
         $('#position-' + i).addClass("even");
@@ -18,6 +22,3 @@ function buildBoard() {
   }
 }
 
-$(document).ready(function() {
-  buildBoard();
-})
